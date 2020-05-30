5e41db3dad42bb46deaa48d381ae47ff69697482
chenwenkai <1165524986@qq.com>
2020-05-30 14:58:02 +0800

    Site updated: 2020-05-30 14:58:01

---
+++ react/precache-manifest.d632940aef96b887269420ef51a30828.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67816f4342dd271c2d037fdc699c6f14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77457b30ad7b794512b0",
+    "url": "/static/css/2.97a13f5c.chunk.css"
+  },
+  {
+    "revision": "77457b30ad7b794512b0",
+    "url": "/static/js/2.c4a48092.chunk.js"
+  },
+  {
+    "revision": "8a616d90534fd98fb212220ba66c3f8a",
+    "url": "/static/js/2.c4a48092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9a34f207418519ef22b",
+    "url": "/static/js/3.f5eb761b.chunk.js"
+  },
+  {
+    "revision": "fe9fe527910a71248a7e",
+    "url": "/static/js/main.b80f1f55.chunk.js"
+  },
+  {
+    "revision": "c474833864a60e7648fa",
+    "url": "/static/js/runtime-main.4c4aa45c.js"
+  }
+]);
