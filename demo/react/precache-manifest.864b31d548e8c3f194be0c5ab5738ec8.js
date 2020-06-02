7a65d29908bb904c3b7f7a36728690dc1f0d3a1a
chenwenkai <1165524986@qq.com>
2020-06-02 20:03:17 +0800

    Site updated: 2020-06-02 20:03:16

---
+++ demo/react/precache-manifest.864b31d548e8c3f194be0c5ab5738ec8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20020abda730a564f984fc45cf3676e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77457b30ad7b794512b0",
+    "url": "/static/css/2.97a13f5c.chunk.css"
+  },
+  {
+    "revision": "77457b30ad7b794512b0",
+    "url": "/static/js/2.c4a48092.chunk.js"
+  },
+  {
+    "revision": "8a616d90534fd98fb212220ba66c3f8a",
+    "url": "/static/js/2.c4a48092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9a34f207418519ef22b",
+    "url": "/static/js/3.f5eb761b.chunk.js"
+  },
+  {
+    "revision": "2339766a6804d54ba4c7",
+    "url": "/static/js/main.925242fb.chunk.js"
+  },
+  {
+    "revision": "c474833864a60e7648fa",
+    "url": "/static/js/runtime-main.4c4aa45c.js"
+  }
+]);
